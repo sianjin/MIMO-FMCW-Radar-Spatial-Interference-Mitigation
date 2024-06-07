e8b96e7b670814d7d54b5739ee6d7a5f422d904a
sianjin <47802717+sianjin@users.noreply.github.com>
2024-06-07 14:02:57 -0400

    Create ReadMe.m in Synthetic Data

---
+++ Synthetic Data/ReadMe.m
@@ -0,0 +1,3 @@
+% This folder contains the file rocSpatial.m to generate comparsion of receiver operating characteristic (ROC) curves of different detectors
+
+% You can use this code to reproduce Fig. 5 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
