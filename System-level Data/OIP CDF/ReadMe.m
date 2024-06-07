22cd22bcc9c6b3234f6e99b88adfb23ed1ecd7cc
sianjin <47802717+sianjin@users.noreply.github.com>
2024-06-07 13:45:06 -0400

    Create ReadMe.m in OIP CDF folder

---
+++ System-level Data/OIP CDF/ReadMe.m
@@ -0,0 +1,7 @@
+% This folder contains the file to generate CDF of output interference power (OIP) over multiple Monte-Carlo runs
+
+% You can use this code to reproduce Fig. 8 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
+
+% The main file to run is setupSim.m, which configures the interference radar and victim radar systems to simulate
+
+% You can change param.numIterSim to set the number of Monte-Carlo runs
