092aa03e7c4ce6945ad89aafed01f5a2c4151ab4
sianjin <47802717+sianjin@users.noreply.github.com>
2024-06-07 13:52:37 -0400

    Create ReadMe.m in angle domain compare folder

---
+++ System-level Data/angle domain compare/ReadMe.m
@@ -0,0 +1,5 @@
+% This folder contains the files to generate qualitative comparsion of angle spectrums of different detectors
+
+% You can use this code to reproduce Fig. 6 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
+
+% The main file to run is setupSim.m, which configures the interference radar and victim radar systems to simulate
