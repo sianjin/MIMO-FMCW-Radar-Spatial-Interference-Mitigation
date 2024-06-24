89a2ccf017a98901b01cfaabb270e71b44200688
sianjin <47802717+sianjin@users.noreply.github.com>
2024-06-24 11:44:48 -0400

    Update ReadMe.m

--- System-level Data/range angle domain compare/ReadMe.m
+++ System-level Data/range angle domain compare/ReadMe.m
@@ -1,5 +1,5 @@
 % This folder contains the files to generate qualitative comparsion of range-angle spectrums of different detectors
 
-% You can use this code to reproduce Fig. 7 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
+% You can use this code to reproduce the performance of clairvoyant, angle FFT, LCMV-SMI, AGS detectors in Fig. 7 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
 
 % The main file to run is setupSim.m, which configures the interference radar and victim radar systems to simulate
