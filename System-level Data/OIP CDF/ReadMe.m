62389d714752a48e02fff726f8c896ae7a32ecdd
sianjin <47802717+sianjin@users.noreply.github.com>
2024-06-24 11:44:06 -0400

    Update ReadMe.m

--- System-level Data/OIP CDF/ReadMe.m
+++ System-level Data/OIP CDF/ReadMe.m
@@ -1,6 +1,6 @@
 % This folder contains the files to generate CDF of output interference power (OIP) over multiple Monte-Carlo runs
 
-% You can use this code to reproduce Fig. 8 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
+% You can use this code to reproduce the performance of clairvoyant, angle FFT, LCMV-SMI, AGS detectors in Fig. 8 in "Spatial-Domain Mutual Interference Mitigation for MIMO-FMCW Automotive Radar"
 
 % The main file to run is setupSim.m, which configures the interference radar and victim radar systems to simulate
 
